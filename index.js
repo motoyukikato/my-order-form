94b686ce3077e7e45b737520cd0aa7771041cea7
motoyukikato <ftshop7087@gmail.com>
2026-09-15 08:20:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,20 @@ export default {
       });
     }
 
-    const [scheme, encoded] = authHeader.split(' ');
-    if (scheme !== 'Basic' || !encoded) {
-      return new Response('Bad Request', { status: 400 });
+    try {
+      const auth = authHeader.split(' ')[1];
+      const [user, pass] = atob(auth).split(':');
+
+      // 設定した環境変数と照合
+      if (user === env.BASIC_USER && pass === env.BASIC_PASS) {
+        // 認証成功時、そのまま要求されたファイル（hanamado.html等）を表示
+        return env.ASSETS.fetch(request);
+      }
+    } catch (e) {
+      // エラー時の処理
     }
 
-    const decoded = atob(encoded);
-    const [user, pass] = decoded.split(':');
-
-    // 設定した環境変数と照合
-    if (user === env.BASIC_USER && pass === env.BASIC_PASS) {
-      return env.ASSETS.fetch(request);
-    }
-
+    // パスワードが一致しない場合
     return new Response('Unauthorized', {
       status: 401,
       headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
